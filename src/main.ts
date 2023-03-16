b777a3d22cb6410776034d1fd6048f5a618512a9
VladimirFomin90 <vladimirfomin90@yandex.ru>
2023-03-17 00:04:45 +0600

    add ListTemplate, FullList

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
 import './css/style.css';
+import FullList from './model/FullList';
+import ListItem from './model/ListItem';
+import ListTemplate from './templates/ListTemplate';
+
+const initApp = (): void => {
+    const fullList = FullList.instance;
+    const template = ListTemplate.instance;
+
+    const itemEntryForm = document.getElementById('itemEntryForm') as HTMLFormElement;
+};
+
+document.addEventListener('DOMContentLoaded', initApp);
